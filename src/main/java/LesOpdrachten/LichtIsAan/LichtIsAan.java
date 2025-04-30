1a01323cc77873957f95430d2850e7a7e101c4ec
Stefan <stefan.kiers89@hotmail.com>
2025-04-30 12:39:12 +0200

    licht is aan

---
+++ src/main/java/LesOpdrachten/LichtIsAan/LichtIsAan.java
@@ -0,0 +1,20 @@
+package LesOpdrachten.LichtIsAan;
+
+/*
+Stefan Kiers
+30-04-'25
+
+Lesopdracht : licht is aan / lightswitch via boolean
+ */
+
+public class LichtIsAan {
+    public static void main(String[] args) {
+        boolean lichtIsAan = false;
+
+        if (lichtIsAan){
+            System.out.println("Light is on");
+        }else {
+            System.out.println("Light is off");
+        }
+    }
+}
