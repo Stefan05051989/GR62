5907f3cc8e733410958d85fb917065364697aa65
Stefan <stefan.kiers89@hotmail.com>
2025-04-30 14:07:22 +0200

    loop

---
+++ src/main/java/LesOpdrachten/LichtIsAan/Loop.java
@@ -0,0 +1,29 @@
+package LesOpdrachten.LichtIsAan;
+
+/*
+Stefan Kiers
+30-04-'25
+
+maak met behulp van een for loop de volgende uitkomst :
+
+123
+234
+345
+456
+567
+678
+789
+
+ */
+
+public class Loop {
+    public static void main(String[] args) {
+        int i = 0;
+
+        for (i = 1; i < 8; i++){
+            System.out.print(i);
+            System.out.print(i + 1);
+            System.out.print(i + 2 + "\n");
+        }
+    }
+}
