1062b23ac449a1c55f15ea74d0a9278a3b3ec32e
Stefan <stefan.kiers89@hotmail.com>
2025-04-30 15:18:22 +0200

    loop schaakbord

--- src/main/java/LesOpdrachten/LichtIsAan/LoopSchaakbord.java
+++ src/main/java/LesOpdrachten/LichtIsAan/LoopSchaakbord.java
@@ -1,5 +1,12 @@
 package LesOpdrachten.LichtIsAan;
 
+/*
+Stefan Kiers
+30-04-'25
+
+Maak een schaakbord met behulp van een loop, startend en eindigend bij 1 / 8.
+ */
+
 public class LoopSchaakbord {
     public static void main(String[] args) {
         int i = 1;
