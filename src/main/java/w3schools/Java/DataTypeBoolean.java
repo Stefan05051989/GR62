a8d2b46e6ec27bb32036a383d0df5a2698554de4
Stefan <stefan.kiers89@hotmail.com>
2025-05-06 13:11:02 +0200

    boolean

---
+++ src/main/java/w3schools/Java/DataTypeBoolean.java
@@ -0,0 +1,26 @@
+package w3schools.Java;
+
+public class DataTypeBoolean {
+    public static void main(String[] args) {
+        /*
+        A boolean data type only had 2 values : true or false.
+        It can be used for confirmation or exclusion, etc.
+         */
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+
+        /*
+        A Boolean does not have a default value, so it has to be set as true or false.
+        Most of the time these are used for conditional testings in methods,
+        if ( a statement is true){
+         do (this method);
+         if not ( do this method)
+         (no correct syntax / method, just to explained it simply for understanding.)
+            }
+         }
+
+         */
+    }
+}
