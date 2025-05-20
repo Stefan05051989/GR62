3964c546f9b2fee75d2650c825a133eec6d28038
Stefan <stefan.kiers89@hotmail.com>
2025-05-20 15:37:48 +0200

    opdracht 18

--- src/main/java/PlayersGuide/Opdracht18/ReplicatorOfDTo.java
+++ src/main/java/PlayersGuide/Opdracht18/ReplicatorOfDTo.java
@@ -26,7 +26,22 @@ public class ReplicatorOfDTo {
         for (int i = 0; i < replicator.length; i++){
             replicator2[i] = replicator[replicator.length - 1 - i];
         }
-        System.out.println("Original Array : " + Arrays.toString(replicator));
+        // Java suggested : String can be replaced with template
+        /*
+        This class contains various methods for manipulating arrays (such as sorting and searching).
+        This class also contains a static factory that allows arrays to be viewed as lists.
+        The methods in this class all throw a NullPointerException, if the specified array reference is null,
+            except where noted.
+        The documentation for the methods contained in this class includes brief descriptions of the implementations.
+        Such descriptions should be regarded as implementation notes, rather than parts of the specification.
+         Implementors should feel free to substitute other algorithms, so long as the specification itself is adhered to.
+        (For example, the algorithm used by sort(Object[]) does not have to be a MergeSort,
+         but it does have to be stable.)
+        This class is a member of the Java Collections Framework.
+         */
+
+        System.out.println(STR."Original Array : \{Arrays.toString(replicator)}");
+        // difference between String template (original) and my version (flipped) :
         System.out.println("Flipped Array : " + Arrays.toString(replicator2));
 
         scanner.close();
